50477c26ba8e53b5acaa38e143d2a25697a1c311
zhiiw <1302593554@qq.com>
2021-05-16 11:58:06 +0800

    enviroment

---
+++ graph.h
@@ -0,0 +1,14 @@
+//
+// Created by zhiiw on 2021/5/16.
+//
+
+#ifndef COVID19GRAPH_INDIA_GRAPH_H
+#define COVID19GRAPH_INDIA_GRAPH_H
+
+
+class graph {
+
+};
+
+
+#endif //COVID19GRAPH_INDIA_GRAPH_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by zhiiw on 2021/5/16.
+//
+#include <iostream>
+int main(){
+    std::cout<<"hello world";
+}
